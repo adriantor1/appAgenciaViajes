f9dfc73a308a4c1a75426d95be6f70ca367540f0
adriantor1 <adriantor@unicauca.edu.co>
2021-03-17 21:22:31 -0500

    Init Commit

---
+++ src/main/java/co/unicauca/tallerpolimorfismo/modelo/Viaje.java
@@ -0,0 +1,68 @@
+package co.unicauca.tallerpolimorfismo.modelo;
+import java.util.Date;
+
+/**
+ * Clase base que imlementa los viajes en general
+ * @author ADRIAN TORRES
+ */
+public abstract class Viaje {
+    protected String origen;
+    protected String destino;
+    protected int costo;
+    protected Date fechaSalida;
+    protected Date fechaLlegada;
+    public abstract String descripcion();
+    public Viaje(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada){
+        this.origen = origen;
+        this.destino = destino;
+        this.costo = costo;
+        this.fechaSalida = fechaSalida;
+        this.fechaLlegada = fechaLlegada;
+    }
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getDestino() {
+        return destino;
+    }
+
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int costo) {
+        this.costo = costo;
+    }
+
+    public Date getFechaSalida() {
+        return fechaSalida;
+    }
+
+    public void setFechaSalida(Date fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    public Date getFechaLlegada() {
+        return fechaLlegada;
+    }
+
+    public void setFechaLlegada(Date fechaLlegada) {
+        this.fechaLlegada = fechaLlegada;
+    }
+    public String cualquierMetodo() {
+        return "Cualquier método implementado en la clase base";
+    }
+    public String cualquierMetodo2() {
+        return "Cualquier método2 implementado en la clase base";
+    }
+}
+
